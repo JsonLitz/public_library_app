948f3e824e3669b1bdbd3cc4c0aa7b3566dfb8d6
jsonlitz <jasondotlee@gmail.com>
2016-08-15 15:01:09 -0700

    added login route and view

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
+  def index
+   @users = User.all
+   render :index
+  end
+
   def new
-    @users = User.new
-    render :index
+    @user = User.new
+    render :new
   end
 
   def create
     user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
-    @user = User.create(user_params)
+    @users = User.create(user_params)
     redirect_to root_path
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
 end
